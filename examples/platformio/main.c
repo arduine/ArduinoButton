56fa063cb3a157709be334b3a48ab40460cfabc3
sollyu <sollyu@qq.com>
2022-03-18 00:05:49 +0800

    :white_check_mark: test code

--- examples/platformio/main.c
+++ examples/platformio/main.c
@@ -1,23 +1,40 @@
-//
-// Created by Sollyu on 2022/3/5.
-//
+/*
+ * Copyright (C) 2022. sollyu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <Arduino.h>
-
 #include <ArduinoButton.h>
 
-ArduinoButton sTestButton(14, INPUT_PULLUP, LOW);
+static ArduinoButton sTestButton(DD2, INPUT_PULLUP, LOW);
 
 void onButtonClick() {
-    Serial.printf("[*] button click %lu\n", millis());
+    digitalWrite(DD3, digitalRead(DD3) == HIGH ? LOW : HIGH);
+}
+
+void onButtonLongClick() {
+    digitalWrite(DD4, digitalRead(DD4) == HIGH ? LOW : HIGH);
 }
 
 void setup() {
-    Serial.begin(115200);
+    pinMode(DD3, OUTPUT);
+    pinMode(DD4, OUTPUT);
+    digitalWrite(DD3, LOW);
+    digitalWrite(DD4, LOW);
+
     sTestButton.setOnClickListener(onButtonClick);
-    sTestButton.setOnLongClickListener([] {
-        Serial.printf("[*] button long click %lu\n", millis());
-    });
+    sTestButton.setOnLongClickListener(onButtonLongClick);
 }
 
 void loop() {
